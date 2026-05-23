e58ff7d244eef7b7cb7ae94f027a5100fa6b00b1
nuhara-web <nuharahanami@gmail.com>
2026-05-23 10:12:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 
+st.set_page_config(
+  page_title="kuliah praktisi 2025",
+  layout="wide"
+)
+
 # Hirarki teks
 st.title("📊 Dashboard")
 st.header("Laporan Bulanan")
 st.subheader("📈 Monthly Expenses")
 st.caption("Made with ❤️ using Streamlit")
-
