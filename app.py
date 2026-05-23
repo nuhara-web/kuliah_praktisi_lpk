33362637932c4e97fc189adda13edb4f3271f222
nuhara-web <nuharahanami@gmail.com>
2026-05-23 10:43:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,16 @@ else:
 
 st.button("Reset", type="primary")
 
+col1, col2, col3 = st.columns(3)
 
 with col1:
     st.header("A cat")
     st.image("https://static.streamlit.io/examples/cat.jpg")
 
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
