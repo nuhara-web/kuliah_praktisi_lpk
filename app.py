e774f95c1a8dfe5a89138bf6d9b7d2b380e2ac78
nuhara-web <nuharahanami@gmail.com>
2026-05-23 10:58:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -47,3 +47,12 @@ with st.expander("See explanation"):
         be random.
     ''')
     st.image("https://static.streamlit.io/examples/dice.jpg")
+
+a, b = st.columns(2)
+c, d = st.columns(2)
+
+a.metric("Temperature", "30°F", "-9°F", border=True)
+b.metric("Wind", "4 mph", "2 mph", border=True)
+
+c.metric("Humidity", "77%", "5%", border=True)
+d.metric("Pressure", "30.34 inHg", "-2 inHg", border=True)
